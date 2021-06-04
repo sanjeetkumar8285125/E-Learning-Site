94ba1ddb8d1ff82749621b00df7f287685d275ec
sanjeetkumar8285125 <sanjeetkumar8285125@gmail.com>
2021-06-04 22:52:10 +0530

    E-Learning

--- routes/Coursepayment.js
+++ routes/Coursepayment.js
@@ -1,14 +1,20 @@
 const express=require('express')
 const router=express.Router();
+const fast2sms=require('fast-two-sms');
 const stripe=require('stripe')('sk_test_51Isr7mSGYjYnomPVVm9owzZDTMe39XeEx3fPE3Ocf3BoapdsOyHfUERbYe5scMBb41bKSyJp2liKzYODfiiYMkRg00dvyRwg9o')
+
+
 router.post('/create-order', (req,res)=>{
- console.log(req.body.price)
+    const CLIENT_URL='http://'+req.headers.host;
+    let message= `Thanks for shopping with us. Your ${req.body.course} Course is purchased Successfully. We will get back to you soon when new bacthes started. Regards E-Learning`
+    var options = {authorization : 'YwrLif1QmVsKC0MNuIztAXqW3lGvD4Sc6HpO5xjU9heoanb28yxGdq2bOUkQuet4niM5NcrKsSjVaygH' , message : message ,  numbers : [`${req.user.phone}`]} 
+ 
   const token=req.body.stripeToken;
   stripe.customers.create({ 
     email: req.body.stripeEmail, 
     name: req.user.name, 
     address: { 
-        line1: 'B-21 Prem Nagr-3', 
+        line1: 'B-21 Prem Nagar-3', 
         postal_code: '110086', 
         city: 'New Delhi', 
         state: 'Delhi', 
@@ -25,8 +31,14 @@ router.post('/create-order', (req,res)=>{
     }); 
 }) 
 .then((charge) => { 
-   req.flash("success_msg","Payment Successfull, Course Purchased Suceessfully") // If no error occurs 
+ 
+    fast2sms.sendMessage(options).then(response=>{
+        console.log(response)
+    })
+   req.flash("success_msg","Payment Successfull, Course Purchased Suceessfully") 
     res.redirect(`/${req.body.course}`)
+  
+
 }) 
 .catch((err) => { 
     res.send(err)    // If some error occurs 
